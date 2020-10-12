64906a59b45c1671391a2cc156c057df8566c65f
William Koller <wkoller25@gmail.com>
2020-10-12 19:44:48 -0300

    feat: ensure getCurrency throw if repository throw

--- src/currencies/currencies.service.spec.ts
+++ src/currencies/currencies.service.spec.ts
@@ -1,18 +1,35 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CurrenciesService } from './currencies.service';
+import { InternalServerErrorException } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import { CurrenciesRepository, CurrenciesService } from './currencies.service'
 
 describe('CurrenciesService', () => {
-  let service: CurrenciesService;
+  let service: CurrenciesService
+  let repository: CurrenciesRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CurrenciesService],
-    }).compile();
+      providers: [
+        CurrenciesService,
+        { provide: CurrenciesRepository, useFactory: () => ({ getCurrency: jest.fn() }) }
+      ],
+    }).compile()
 
-    service = module.get<CurrenciesService>(CurrenciesService);
-  });
+    service = module.get<CurrenciesService>(CurrenciesService)
+    repository = module.get<CurrenciesRepository>(CurrenciesRepository)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+
+  describe('getCurrency()', () => {
+    it('should be throw if repository throw', async () => {
+      (repository.getCurrency as jest.Mock).mockRejectedValue(new InternalServerErrorException())
+      await expect(service.getCurrency('INVALID')).rejects.toThrow(new InternalServerErrorException())
+    })
+
+    it('should be no throw if repository returns', async () => {
+      await expect(service.getCurrency('USD')).resolves.not.toThrow()
+    })
+  })
+})
--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common'
+
+export class CurrenciesRepository {
+    async getCurrency(currency: string): Promise<any> {
+
+    }
+}
+
 
 @Injectable()
 export class CurrenciesService {
-    async getCurrency(currency: string): Promise<any> { }
+    constructor(private currenciesRepository: CurrenciesRepository) { }
+    async getCurrency(currency: string): Promise<any> {
+        try {
+            await this.currenciesRepository.getCurrency(currency)
+        } catch (error) {
+            throw new InternalServerErrorException()
+        }
+    }
 }

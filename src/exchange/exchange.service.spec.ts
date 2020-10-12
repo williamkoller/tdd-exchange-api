369ebe00760e8e657ff574bd62eb4eb3aa6d170c
William Koller <wkoller25@gmail.com>
2020-10-12 04:10:35 -0300

    feat: ensure convertAmount called getcurrency twice

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,16 +1,25 @@
 import { BadRequestException } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
-import { ExchangeService } from './exchange.service'
+import { CurrenciesService, ExchangeService } from './exchange.service'
 
 describe('ExchangeService', () => {
   let service: ExchangeService
+  let currenciesService: CurrenciesService
+
 
   beforeEach(async () => {
+    const currenciesServiceMock = {
+      getCurrency: jest.fn()
+    }
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
+      providers: [
+        ExchangeService,
+        { provide: CurrenciesService, useFactory: () => currenciesServiceMock }],
     }).compile()
 
     service = module.get<ExchangeService>(ExchangeService)
+    currenciesService = module.get<CurrenciesService>(CurrenciesService)
+
   })
 
   it('should be defined', () => {
@@ -19,11 +28,22 @@ describe('ExchangeService', () => {
 
   describe('ConvertAmount()', () => {
     it('should be throw if called with invalid params', async () => {
-      await expect(service.convertAmount({ from: '', to: '', amount: 0 })).rejects.toThrow(new BadRequestException())
+      await expect(service.convertAmount({ from: '', to: '', amount: 0 }))
+        .rejects.toThrow(new BadRequestException())
     })
 
     it('should be not throw if called with valid params', async () => {
-      await expect(service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 })).resolves.not.toThrow()
+      await expect(service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 }))
+        .resolves.not.toThrow()
+    })
+
+    it('should be called getCurrency twice', async () => {
+      await service.convertAmount({
+        from: 'USD',
+        to: 'BRL',
+        amount: 1
+      })
+      await expect(currenciesService.getCurrency).toBeCalledTimes(2)
     })
   })
 })

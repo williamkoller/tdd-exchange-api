6cc9660c4a99e618540596f0b0f746ae30561075
William Koller <wkoller25@gmail.com>
2020-10-12 03:02:00 -0300

    feat: ensure convertAmount not throw if called with valid params

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
 import { ExchangeService } from './exchange.service'
 
@@ -18,7 +19,11 @@ describe('ExchangeService', () => {
 
   describe('ConvertAmount()', () => {
     it('should be throw if called with invalid params', async () => {
-      await expect(service.convertAmount({ from: '', to: '', amount: 0 })).rejects.toThrow()
+      await expect(service.convertAmount({ from: '', to: '', amount: 0 })).rejects.toThrow(new BadRequestException())
+    })
+
+    it('should be not throw if called with valid params', async () => {
+      await expect(service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 })).resolves.not.toThrow()
     })
   })
 })

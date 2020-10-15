50fbf861efa5e12e5d94fba4ab8fd19a077c2c8c
William Koller <wkoller25@gmail.com>
2020-10-15 00:30:54 -0300

    feat: ensure getCurrency be  called findOne with correct params

--- src/currencies/currencies.repository.ts
+++ src/currencies/currencies.repository.ts
@@ -1,8 +1,15 @@
-import { Currencies } from './currency.entities'
+import { EntityRepository, Repository } from 'typeorm'
+import { Currencies } from './currencies.entities'
+import { InternalServerErrorException } from '@nestjs/common';
 
-export class CurrenciesRepository {
+@EntityRepository()
+export class CurrenciesRepository extends Repository<Currencies> {
     async getCurrency(currency: string): Promise<Currencies> {
-        return new Currencies()
+        const result = await this.findOne({ currency })
+        if(!result) {
+            throw new InternalServerErrorException()
+        }
+        return result
     }
 
     async createCurrency({ currency, value }): Promise<Currencies> {

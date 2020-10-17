3662ee8c29e221621ed850423491e6b510acabc2
William Koller <wkoller25@gmail.com>
2020-10-16 22:37:50 -0300

    feat: ensure createCurrency called with correct params

--- src/currencies/currencies.repository.ts
+++ src/currencies/currencies.repository.ts
@@ -1,25 +1,29 @@
 import { EntityRepository, Repository } from 'typeorm'
 import { Currencies } from './currencies.entities'
-import { InternalServerErrorException } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common'
+import { CurrenciesInputType } from './types/currencies-input.type'
 
 @EntityRepository()
 export class CurrenciesRepository extends Repository<Currencies> {
     async getCurrency(currency: string): Promise<Currencies> {
         const result = await this.findOne({ currency })
-        if(!result) {
+        if (!result) {
             throw new InternalServerErrorException()
         }
         return result
     }
 
-    async createCurrency({ currency, value }): Promise<Currencies> {
+    async createCurrency(currenciesInputType: CurrenciesInputType): Promise<Currencies> {
+        const createCurrency = new Currencies()
+        Object.assign(createCurrency, currenciesInputType)
+
+        await this.save(createCurrency)
         return new Currencies()
     }
 
-    async updateCurrency({ currency, value }): Promise<Currencies> {
+    async updateCurrency({ currency, value }: CurrenciesInputType): Promise<Currencies> {
         return new Currencies()
     }
-
     async deleteCurrency(currency: string): Promise<void> {
         return
     }

464d28b866a70ae1ec9037135b59f3b8d36c4947
William Koller <wkoller25@gmail.com>
2020-10-16 22:56:48 -0300

    refactor: add validation fields

--- src/currencies/currencies.entities.ts
+++ src/currencies/currencies.entities.ts
@@ -1,3 +1,5 @@
+import { Type } from 'class-transformer'
+import { IsNotEmpty, IsNumber, Length } from 'class-validator'
 import { Column, CreateDateColumn, Entity, ObjectIdColumn, PrimaryColumn, Unique, UpdateDateColumn } from 'typeorm'
 
 @Unique(['currency'])
@@ -7,9 +9,14 @@ export class Currencies {
     _id: string
 
     @PrimaryColumn()
+    @Length(3, 3)
+    @IsNotEmpty()
     currency: string
 
     @Column()
+    @IsNotEmpty()
+    @IsNumber()
+    @Type(() => Number)
     value: number
 
     @CreateDateColumn({ type: 'timestamp' })

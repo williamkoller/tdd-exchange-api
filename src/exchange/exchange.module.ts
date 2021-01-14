a0dd101559558ef89e8747935f4787e0f40ce4e4
william koller <wkoller25@gmail.com>
2021-01-14 00:59:25 -0300

    feat: set CurrenciesModule

--- src/exchange/exchange.module.ts
+++ src/exchange/exchange.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CurrenciesModule } from 'src/currencies/currencies.module';
 import { ExchangeService } from './services/exchange.service';
 
 @Module({
+  imports: [CurrenciesModule],
   providers: [ExchangeService],
 })
 export class ExchangeModule {}
